eaef0736d6caf3117572ac2a3fa504a07c3e0c95
SVLucas <120425932+SVLucasAndres@users.noreply.github.com>
2026-06-29 22:59:07 -0500

    Revert "Unificar el Dominio"

--- src/main/java/com/ucuenca/proyecto_courier/CapaDominio/Excepciones/OperacionInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package com.ucuenca.proyecto_courier.CapaDominio.Excepciones;
-
-public class OperacionInvalidaException extends RuntimeException {
-    public OperacionInvalidaException(String mensaje) {
-        super(mensaje);
-    }
-}
